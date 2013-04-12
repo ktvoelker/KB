d65c92771332f993c034b74ebed762b16afc13ba
Karl Voelker <ktvoelker@gmail.com>
2013-04-11 23:07:22 -0700

    More note API functions

--- web/kb.py
+++ web/kb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from bottle import request, response, route, static_file, run
+from bottle import request, response, route, static_file, run, abort
 
 import configparser
 import json
@@ -10,6 +10,10 @@ import psycopg2
 CONFIG = configparser.ConfigParser()
 CONFIG.read("kb.ini")
 
+SOCKETS = os.getcwd() + "/db/pg"
+
+NOTE_FIELDS = ["title", "body"]
+
 @route("/")
 def home():
   return static("index.html")
@@ -18,23 +22,23 @@ def home():
 def static(filename):
   return static_file(filename, root="static")
 
-def connect():
-  sockets = os.getcwd() + "/db/sockets"
-  return psycopg2.connect(database="kb", user="web", host=sockets)
+def transact(fn):
+  def result(*args, **kwargs):
+    with psycopg2.connect(database="kb", user="web", host=SOCKETS) as conn:
+      with conn.cursor() as cur:
+        return fn(cur, *args, **kwargs)
+  return result
 
 @route("/authenticate", method="POST")
-def authenticate():
+@transact
+def authenticate(c):
   name = request.forms.name
   password = request.forms.password
-  conn = connect()
-  c = conn.cursor()
   c.execute(
       "SELECT id FROM kb.person WHERE name = %s AND password = crypt(%s, password)",
       (name, password))
-  row = c.fetchone()
-  c.close()
-  conn.close()
-  if row:
+  if c.rowcount == 1:
+    row = c.fetchone()
     response.set_cookie("person_id", row[0], secret=CONFIG["global"]["secret"])
     return None
   else:
@@ -42,39 +46,76 @@ def authenticate():
 
 def person_id(mandatory=True):
   ret = request.get_cookie("person_id", secret=CONFIG["global"]["secret"])
-  if ret is None:
+  if mandatory and ret is None:
     abort(401)
   return ret
 
 @route("/note/:id")
-def note(id):
+@transact
+def note_get(c, id):
   pid = person_id()
-  conn = connect()
-  c = conn.cursor()
   c.execute("""
     SELECT title, body, created
     FROM kb.note
     WHERE owner = %s
     AND id = %s
     """, (pid, id))
-  row = c.fetchone()
-  if row:
+  if c.rowcount == 1:
+    row = c.fetchone()
     ret = {"id": id, "title": row[0], "body": row[1], "created": row[2].isoformat()}
-    c.close()
-    conn.close()
     return ret
   else:
     abort(404)
 
+@route("/note/new", method="POST")
+@transact
+def note_new(c):
+  pid = person_id()
+  fields = ["owner"]
+  slots = ["%%s"]
+  params = [pid]
+  for field in NOTE_FIELDS:
+    if not request.forms[field]:
+      abort(400)
+    fields.append(field)
+    slots.append("%%s")
+    params.append(request.forms.getunicode(field))
+  c.execute(
+      "INSERT INTO kb.note (" + ", ".join(fields) + ") VALUES (" +
+      ", ".join(slots) + ") RETURNING id", params)
+  if c.rowcount == 1:
+    (id,) = c.fetchone()
+    return {"id": id}
+  else:
+    abort(500)
+
+@route("/note/:id", method="POST")
+@transact
+def note_edit(c, id):
+  pid = person_id()
+  changes = []
+  params = []
+  for field in NOTE_FIELDS:
+    val = request.forms.getunicode(field)
+    if val:
+      changes.append("%s = %%s" % (field,))
+      params.append(val)
+  if len(changes) > 0:
+    params.append(pid)
+    params.append(id)
+    c.execute(
+        "UPDATE kb.note SET " + ", ".join(changes) +
+        "WHERE owner = %s AND id = %s", params)
+  return None
+
 @route("/note/list")
-def note_list():
+@transact
+def note_list(c):
   query = request.query.query
   if query:
     abort(400)
   else:
     pid = person_id()
-    conn = connect()
-    c = conn.cursor()
     c.execute("""
       SELECT id, title, created
       FROM kb.note
@@ -83,8 +124,6 @@ def note_list():
       LIMIT %s
       """, (pid, CONFIG["global"]["query-limit"]))
     ret = [{"id": row[0], "title": row[1], "created": row[2].isoformat()} for row in c]
-    c.close()
-    conn.close()
     return {"notes": ret}
 
 if __name__ == "__main__":
